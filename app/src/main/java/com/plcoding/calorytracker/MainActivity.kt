e989226edc0dcab01f777029052d6ab1968d79db
Miguel-Silva97 <migueel.silvaa97@gmail.com>
2024-02-05 21:46:58 +0000

    Fix Imports

--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -3,13 +3,6 @@ package com.example.calorytracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.compiler.plugins.kotlin.EmptyFunctionMetrics.composable
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavHost
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
